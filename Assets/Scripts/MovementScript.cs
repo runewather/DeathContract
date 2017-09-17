de3fb451b140fb42af4a6249c0a89388b2c11704
victo <victo@DESKTOP-5RH1MK1>
2017-09-17 03:48:45 -0300

    init

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour {
+
+    private Rigidbody rb;
+    private float horizontalInput;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    void HorizontalMovement()
+    {
+        rb.MovePosition(rb.position);
+    }
+
+	void FixedUpdate ()
+    {
+	    	
+	}
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+}

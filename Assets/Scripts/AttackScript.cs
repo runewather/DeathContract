f4b8d9536cf79bb1166a32fb4134dee99bbf2f51
victo <victo@DESKTOP-5RH1MK1>
2017-09-18 02:27:09 -0300

    Slime and platforms

--- Assets/Scripts/AttackScript.cs
+++ Assets/Scripts/AttackScript.cs
@@ -5,14 +5,30 @@ using UnityEngine;
 public class AttackScript : MonoBehaviour {
 
     private Animator anim;
-    
+
+    [SerializeField]
+    private WeaponScript scythe;
+
+    public void EnableAttack()
+    {
+        scythe.EnableAttack();
+    }
+
+    public void DisableAttack()
+    {
+        scythe.DisableAttack();
+    }
+
     void Start()
     {
-
+        anim = GetComponent<Animator>();
     }
 
 	void Update ()
     {
-		
+		if(Input.GetButtonDown("Attack"))
+        {
+            anim.SetTrigger("Attack");
+        }
 	}
 }
